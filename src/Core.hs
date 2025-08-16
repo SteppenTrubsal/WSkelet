c3537990d8c147f0b4b562e034ea1e1c946798a8
Niko <n1kokara@yandex.ru>
2025-08-16 16:15:23 +0300

    Env and server essencials

--- src/Core.hs
+++ src/Core.hs
@@ -1,1 +1,20 @@
-module Core where
+module Core(
+  launch
+) where
+
+import           Network.Wai                    (Application)
+import qualified Network.Wai.Handler.Warp       as WW
+import qualified Network.Wai.Handler.WebSockets as WWS
+import qualified Network.WebSockets             as WS
+
+import           Auth (withUser)
+import           Core.Env
+
+app :: Env -> Application
+app env = withUser env $ WWS.websocketsOr WS.defaultConnectionOptions
+
+launch :: IO ()
+launch = do
+  env <- initEnv
+  putStrLn "Running..."
+  WW.run 8080 (app env)
